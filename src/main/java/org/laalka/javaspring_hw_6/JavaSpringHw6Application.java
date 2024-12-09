18fc355a79486c61fd9ad5cba008695fe366e849
LaaLkA <n.afonchenkov@yandex.ru>
2024-12-09 16:27:39 +0300

    Initial commit

---
+++ src/main/java/org/laalka/javaspring_hw_6/JavaSpringHw6Application.java
@@ -0,0 +1,13 @@
+package org.laalka.javaspring_hw_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHw6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringHw6Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/laalka/javaspring_hw_6/JavaSpringHw6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laalka.javaspring_hw_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringHw6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
